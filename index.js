af920aae80e9a5d083e6121a8f3de3c2e47955c2
Yash Pathak <144787943+YashPathak1110@users.noreply.github.com>
2023-09-26 19:07:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,204 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import "./index.css";
+ReactDOM.render(
+  <>
+  <div className="div1">
+ <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="y3"><h2 className="h2"><b>BIERGUT PREMIUM BEER</b></h2><br />
+              <h1 style={{ fontFamily: "Fixedsys", fontSize: "65px" }}><b>INTRODUCING NEW PREMIUM FLAVOURS</b></h1>
+              <p style={{ fontSize: "18px" }}>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of 'dolorem ipsum' ('pain itself'). </p>
+              <button type="button" class="btn btn-dark" style={{ marginLeft: 15, marginTop: "3%", fontSize: "23px", border: 1, boxShadow: "6px 6px 6px white", fontFamily: "System" }}>HOST A PARTY</button>
+              <button type="button" class="btn btn-secondary" style={{ marginLeft: 25, marginTop: "3%", fontSize: "23px", border: 1, boxShadow: "6px 6px 6px white", fontFamily: "System", color: "black" }}>RESTOCK YOUR PUB</button>
+            </div>
+          </div>
+
+
+          <div className="col-sm-6">
+            <img src="https://o.remove.bg/downloads/84fe3d19-6e3a-443c-a7a6-3c67f324460d/92f2e49600bb8b401685cd103724576d-removebg-preview.png" style={{marginLeft: "7rem", marginTop: "8rem"}}  ></img>
+          </div>
+        </div>
+      </div>
+      </div>
+    
+      <div className="div2">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+          <img src="https://wallpaper.dog/large/20564086.jpg" width={"600px"} height={"400px"} className="img2"></img>
+
+          </div>
+
+          <br></br>
+          <div className="col-sm-6">
+          <div className="y3"><h2 className="h2-2"><b>LEADING BEER BREWERY,<br></br> SINCE 1974!</b></h2><br />
+              <p style={{ fontSize: "18px" }}>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of 'dolorem ipsum' ('pain itself'). </p>
+              <button type="button" class="btn btn-light" style={{ marginLeft: 15, marginTop: "3%", fontSize: "23px", border: 1, boxShadow: "9px 13px 13px gray", fontFamily: "System" }}>HOST A PARTY</button>
+              <button type="button" class="btn btn-secondary" style={{ marginLeft: 25, marginTop: "3%", fontSize: "23px", border: 1, boxShadow: "6px 6px 6px white", fontFamily: "System", color: "black" }}>RESTOCK YOUR PUB</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+        <div className="container">
+          <div className="row">
+              <div className="col-3">
+              <h1 className="h1-2"><b>1974</b></h1>
+              <hr className="hr-1" ></hr>
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et  a 1-century BC text by the Roman states.</p>
+              </div>
+              <div className="col-3">
+              <h1 className="h1-2"><b>1985</b></h1>
+              <hr className="hr-1" ></hr>
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et  a 1-century BC text by the Roman states.</p>
+
+              </div>
+              <div className="col-3">
+              <h1 className="h1-2"><b>2005</b></h1>
+              <hr className="hr-1" ></hr>
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et  a 1-century BC text by the Roman states.</p>
+
+              </div>
+              <div className="col-3">
+              <h1 className="h1-2"><b>2023</b></h1>
+              <hr className="hr-1" ></hr>
+              <p>Lorem ipsum is typically a corrupted version of De finibus bonorum et  a 1-century BC text by the Roman states.</p>
+
+              </div>
+          </div>
+        </div>
+      </div>
+                        <br></br><br></br>
+           <div classNa me="div3">
+            <h1 className="h1-3"><b>DISCOVER OUR<br></br> NEW FEATURES</b></h1>
+            <p className="p-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+           
+            <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+          <img src="https://o.remove.bg/downloads/3212b864-699b-4b4e-b7e8-207ae9604246/pngtree-beer-glassware-bayerisch-bierglas-beer-garden-photo-image_7777046-removebg-preview.png"   style={{marginLeft: "10rem"}}></img>
+
+          </div>
+
+         
+          <div className="col-sm-6">
+          <div className="div4">
+          <div style={{ marginTop: "5rem"}}>
+          <h5 className="h5"><b>BIERGUT</b></h5>
+          <h1 style={{fontFamily: "Britannic Bold", color: "orange", marginLeft: "2.5rem"}}><b>PREMIUM LIGHT</b></h1><br></br>
+          <p className="p-4">It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.<br></br></p>
+            </div><br></br>
+          
+
+          <div className="container" >
+            <div className="row">
+                <div className="col-sm-6">
+                  <p style={{fontFamily: "Cooper Black", marginLeft: "1rem"}}><b> EXTRACT</b></p>
+                  <p  style={{marginLeft: "1rem"}}>11%</p><br></br>
+                  <p style={{fontFamily: "Cooper Black", marginLeft: "1rem" }}><b>GENTAIN</b></p>
+                  <p style={{marginLeft: "1rem"}}>20IBU</p>
+                </div>
+
+                <div className="col-sm-6">
+                    <p style={{fontFamily: "Cooper Black", marginLeft: "1rem"}}><b>ALCOHOL</b></p>
+                    <p style={{marginLeft: "1rem"}}>4%</p><br></br>
+                    <p style={{fontFamily: "Cooper Black", marginLeft: "1rem"}}><b>SERVING TEMP</b></p>
+                    <p style={{marginLeft: "1rem"}}>6-9 Degree Celcius</p>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+          <img src="https://o.remove.bg/downloads/ab0f67ee-8a61-419b-8443-8766fa1edbd7/Beer_Closeup_White_background_Mug_Foam_558925_1365x1024-removebg-preview.png" className="img5"></img>
+
+          </div>
+          
+
+          <div className="col-sm-6">
+          <div className="div5">
+          <div style={{ marginTop: "5rem"}}>
+          <h5 className="h5"><b>BIERGUT</b></h5><br></br>
+          <h1 style={{fontFamily: "Britannic Bold", color: "red", marginLeft: "2.5rem"}}><b>PREMIUM LIGHT</b></h1><br></br>
+          <p className="p-4">It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.<br></br></p>
+          <p className="p-4">It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.<br></br></p>
+
+            </div><br></br>
+            </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+        
+        <br></br><br></br>
+        <div className="div6">
+        <div className="div3"><br></br><br></br>
+            <h1 className="h1-3"><b>FEATURES&<br></br> INGREDIENTS</b></h1>
+            <p className="p-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+           
+            <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+          <img src="https://o.remove.bg/downloads/c727f42b-8f8a-4d27-9a91-3660d00c3d71/Screenshot_2023-09-26_174126-removebg-preview.png"   style={{marginLeft: "10rem"}}></img>
+
+          </div>
+
+         
+          <div className="col-sm-6"><br></br><br></br><br></br>
+            <p><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5Z"/>
+  <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
+</svg> It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.</p>
+            
+            
+            <p><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-flower3" viewBox="0 0 16 16">
+  <path d="M11.424 8c.437-.052.811-.136 1.04-.268a2 2 0 0 0-2-3.464c-.229.132-.489.414-.752.767C9.886 4.63 10 4.264 10 4a2 2 0 1 0-4 0c0 .264.114.63.288 1.035-.263-.353-.523-.635-.752-.767a2 2 0 0 0-2 3.464c.229.132.603.216 1.04.268-.437.052-.811.136-1.04.268a2 2 0 1 0 2 3.464c.229-.132.489-.414.752-.767C6.114 11.37 6 11.736 6 12a2 2 0 1 0 4 0c0-.264-.114-.63-.288-1.035.263.353.523.635.752.767a2 2 0 1 0 2-3.464c-.229-.132-.603-.216-1.04-.268zM9 4a1.468 1.468 0 0 1-.045.205c-.039.132-.1.295-.183.484a12.88 12.88 0 0 1-.637 1.223L8 6.142a21.73 21.73 0 0 1-.135-.23 12.88 12.88 0 0 1-.637-1.223 4.216 4.216 0 0 1-.183-.484A1.473 1.473 0 0 1 7 4a1 1 0 1 1 2 0zM3.67 5.5a1 1 0 0 1 1.366-.366 1.472 1.472 0 0 1 .156.142c.094.1.204.233.326.4.245.333.502.747.742 1.163l.13.232a21.86 21.86 0 0 1-.265.002 12.88 12.88 0 0 1-1.379-.06 4.214 4.214 0 0 1-.51-.083 1.47 1.47 0 0 1-.2-.064A1 1 0 0 1 3.67 5.5zm1.366 5.366a1 1 0 0 1-1-1.732c.001 0 .016-.008.047-.02.037-.013.087-.028.153-.044.134-.032.305-.06.51-.083a12.88 12.88 0 0 1 1.379-.06c.09 0 .178 0 .266.002a21.82 21.82 0 0 1-.131.232c-.24.416-.497.83-.742 1.163a4.1 4.1 0 0 1-.327.4 1.483 1.483 0 0 1-.155.142zM9 12a1 1 0 0 1-2 0 1.476 1.476 0 0 1 .045-.206c.039-.131.1-.294.183-.483.166-.378.396-.808.637-1.223L8 9.858l.135.23c.241.415.47.845.637 1.223.083.19.144.352.183.484A1.338 1.338 0 0 1 9 12zm3.33-6.5a1 1 0 0 1-.366 1.366 1.478 1.478 0 0 1-.2.064c-.134.032-.305.06-.51.083-.412.045-.898.061-1.379.06-.09 0-.178 0-.266-.002l.131-.232c.24-.416.497-.83.742-1.163a4.1 4.1 0 0 1 .327-.4c.046-.05.085-.086.114-.11.026-.022.04-.03.041-.032a1 1 0 0 1 1.366.366zm-1.366 5.366a1.494 1.494 0 0 1-.155-.141 4.225 4.225 0 0 1-.327-.4A12.88 12.88 0 0 1 9.74 9.16a22 22 0 0 1-.13-.232l.265-.002c.48-.001.967.015 1.379.06.205.023.376.051.51.083.066.016.116.031.153.044l.048.02a1 1 0 1 1-1 1.732zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+</svg>  It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.</p>
+            
+            <p><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bookmark-star" viewBox="0 0 16 16">
+  <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+</svg> It is a long established fact that a reader will<br></br> be distracted by the readable content of page<br></br> when looking at its layout. The point of using<br></br> Lorem Ipsum is that it has a more-less-or an<br></br> normal distribution of letters, as opposed to.</p>
+
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <div className="div7">
+          <div className="opacity">
+           </div>
+          <h1 className="h1-4"><b>DISCOVER THE<br></br>BREWING PROCESS</b></h1>
+          <p className="p-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-play-btn svg" viewBox="0 0 16 16">
+  <path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+</svg>
+          </div>
+          <div className="container">
+            <div className="row">
+              
+                <div className="col-sm-6">
+                    <img src="https://media.glamour.com/photos/616c4536100a675b50668364/4:3/w_4551,h_3413,c_limit/host-a-party-getty-101721.jpg" width={"970px"} height={"700px"} className="img-4" ></img>
+                </div>
+
+                <div className="col-sm-6">
+                  <img src="https://i0.wp.com/www.musclesandtussles.com/wp-content/uploads/2019/06/antenna-502693-unsplash.jpg?resize=665%2C435&ssl=1" width={"970px"} height={"700px"}></img>
+                </div>
+            </div>
+
+          </div>
+        
+        </>
+  ,
+  document.getElementById("root")
+);
+
